6438bf4b66dd3077f79374b0939e2e897dafe3f6
d.tolmeneva <d.tolmeneva@innopolis.university>
2025-03-03 00:13:46 +0300

    fix: problems with static files

--- stripe_project/settings.py
+++ stripe_project/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,7 +74,7 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
